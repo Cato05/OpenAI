0123fdfeb443b53d89cde75d277bedf868abe4f1
Cato <szmarton08@gmail.com>
2024-08-18 13:17:37 +0200

    API basics, chatbot

---
+++ chatbot/main.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+
+
+def main():
+    client = OpenAI(api_key=os.getenv("apiKey"))
+
+    message = input("User: ")
+    system = "You are a helpful assistent"
+    while message != "":
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+            {"role" : "system", "content" : system},
+            {"role": "user", "content" : message}
+            ]
+        )
+
+        #Extracting the generated text from response
+        generatedText = response.choices[0].message.content
+
+        print(generatedText)
+        message = input("User: ")
+    print("=====================================================\n End")
