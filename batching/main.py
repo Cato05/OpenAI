70cec933444e20cb920c789c9d1f0a9aab676ad5
Cato <szmarton08@gmail.com>
2024-08-18 14:22:31 +0200

    Starting with batches

--- batching/main.py
+++ batching/main.py
@@ -2,9 +2,23 @@ import openai
 from dotenv import load_dotenv
 import os
 
+#Loading .env file
 def configure():
     load_dotenv()
 
 def main():
     configure()
+    #Creating client
+    client = openai.OpenAI(api_key=os.getenv("apiKey"))
+    batchInputFile = os.getenv("batchInputFileID")
 
+    client.batches.create(
+        input_file_id = batchInputFile,
+        endpoint = "/v1/chat/completions",
+        completion_window = "24h",
+        metadata ={
+            "description":"nightly eval job"
+        }
+    )
+
+main()
--- chatbot/main.py
+++ chatbot/main.py
@@ -1,19 +1,23 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-
+#Loading .env file
 def configure():
     load_dotenv()
 
 
 def main():
     configure()
+    #Making a client
     client = OpenAI(api_key=os.getenv("apiKey"))
 
+    #Creating the user input
     message = input("User: ")
     system = "You are a helpful assistent"
+    #Generating answer
     while message != "":
         response = client.chat.completions.create(
+            #Specifing model
             model="gpt-4o-mini",
             messages=[
             {"role" : "system", "content" : system},
@@ -23,7 +27,7 @@ def main():
 
         #Extracting the generated text from response
         generatedText = response.choices[0].message.content
-
+        #Printing response
         print(generatedText)
         message = input("User: ")
     print("=====================================================\n End")
