1690497cfe5538859072cfafa51e15f34c81bd40
Cato <szmarton08@gmail.com>
2024-08-19 09:46:22 +0200

    IMDB top 1000 movies from 2020 categorized. Next step is creating the batch from this input file

--- batching/main.py
+++ batching/main.py
@@ -1,24 +1,69 @@
 import openai
 from dotenv import load_dotenv
+import pandas as pd
+import json
 import os
 
 #Loading .env file
-def configure():
-    load_dotenv()
+load_dotenv()
+
+#Variables
+datasetPath = "./data/IMDB top 1000.csv"
+df = pd.read_csv(datasetPath)
+df.head()
+#Creating client
+client = openai.OpenAI(api_key=os.getenv("apiKey"))
+system = '''Your goal is to extract every movie from the given dataset and categorise them by these terms:
+    1.: The format must be a json object, like this: {
+    categories: ['category1', 'category2', 'category3'],
+    summary: '1-sentence summary'
+        }
+    2.: For categories choose the most relevant genres and don't give more than 4 categorie to a movie.
+    3.: Summerise the movie's description into a one sentence summary'''
+
+
+def getGenres(description):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        temperature=0.1,
+        response_format={
+            "type" : "json_object"
+        },
+        messages=[
+            {"role" : "system", "content" : system},
+            {"role" : "user", "content" : description}
+        ]
+    )
+    return response.choices[0].message.content
+
+
 
 def main():
-    configure()
-    #Creating client
-    client = openai.OpenAI(api_key=os.getenv("apiKey"))
-    batchInputFile = os.getenv("batchInputFileID")
+    for _,row in df[:5].iterrows():
+        description = row['Description']
+        title = row['Title']
+        result = getGenres(description)
+        print(f"Title \t Overview \t Result \t")
+        print("====================================")
+        print(f"{title} \n {description} \n {result}")
 
-    client.batches.create(
-        input_file_id = batchInputFile,
-        endpoint = "/v1/chat/completions",
-        completion_window = "24h",
-        metadata ={
-            "description":"nightly eval job"
-        }
-    )
+
+
+
+
+
+
+
+
+
+
+
+#Description
+#We want a program to classify every from the dataset
+#Format: json
+#Syntax: {
+#    categories: ['category1', 'category2', 'category3'],
+#    summary: '1-sentence summary'
+#        } 
 
 main()
